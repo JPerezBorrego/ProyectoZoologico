dddd43aaf40482bef555880bcc6714fdee8facd0
JPerezBorrego <145455605+JPerezBorrego@users.noreply.github.com>
2023-12-10 19:49:11 +0100

    Proyecto zoo

---
+++ main/src/main/java/com/zoologico/main/controller/ImportacionController.java
@@ -0,0 +1,31 @@
+package com.zoologico.main.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.zoologico.main.service.BBDDservicio;
+@Controller
+public class ImportacionController {
+@Autowired
+    private final BBDDservicio bbddServicios;
+
+    public ImportacionController(BBDDservicio bbddServicios) {
+        this.bbddServicios = bbddServicios;
+    }
+
+    @ResponseBody
+    @GetMapping("/datosCSV")
+    public String importarDatosCSV() {
+        bbddServicios.importarDatosCSV();
+        return "Importación de datos CSV realizada2";
+    }
+
+}
+
+//controladores servicios igual
+//objeto con cada "tabla"
+//repositorio base de datos
+//controlador controla la llamada (siempre llama al servicio (no tiene "nada"))
+//servicios todos los metodos que no tengan que ver con base de datos (si necesitamos ver base de datos se llama a repositorio)
